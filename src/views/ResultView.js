9eba6502bef530f23c40fb6277843fb6ed1014a6
Maxbwee <maxbowen1234@gmail.com>
2022-09-21 11:54:43 +0300

    Add styles to progressbar

--- src/views/ResultView.js
+++ src/views/ResultView.js
@@ -2,8 +2,15 @@ import { useState } from "react";
 import testData from "../testData";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import ProgressBar from "@ramonak/react-progress-bar";
+import { borderColor, width } from "@mui/system";
 
 export default function () {
+  const styles = {
+    section: {
+      backgroundColor: "blue",
+    },
+  };
+
   return (
     <>
       <Grid2
@@ -12,7 +19,15 @@ export default function () {
         columnSpacing={{ xs: 1, sm: 2, md: 3 }}
         spacing={2}
         border={2}
-        style={{ padding: 2, margin: "auto", width: "80%", marginTop: 20 }}
+        style={{
+          padding: 2,
+          margin: "auto",
+          width: "80%",
+          marginTop: 20,
+          backgroundColor: "#919189",
+          padding: 10,
+          borderRadius: 20,
+        }}
       >
         {testData.map((prog) => {
           const progress = (prog.allocatedHours / prog.requiredHours) * 100;
@@ -24,8 +39,11 @@ export default function () {
               <Grid2 xs={3}>{prog.name}</Grid2>
               <Grid2 xs={3}>
                 <ProgressBar
+                  style={styles.section}
+                  baseBgColor={"black"}
                   labelColor={"black"}
                   bgColor={color}
+                  padding={"3px"}
                   completed={progress}
                 />
               </Grid2>
