01d8025a50e71168cf60349d328df7acc8462597
Jaakko Aakula <jaakko.aakula@gmail.com>
2022-11-09 10:00:23 +0200

    resultRoomsStore file added to fetch result testdata from backend

---
+++ src/data/ResultRoomsStore.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import React from 'react'
+
+
+    class ResultRoomsStore {
+
+        getRooms() {
+            return this.rooms;
+        }
+
+        fetchRooms(id) {
+           axios.get(`http://localhost:3001/api/allocation/${id}/rooms`)
+           .then(data => this.rooms = data.data)
+           .catch(e => console.error(e));
+        }
+    }
+
+    const resultRoomsStore = new ResultRoomsStore();
+    export default resultRoomsStore;
