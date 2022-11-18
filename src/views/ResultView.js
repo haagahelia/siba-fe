9e8335f0d1f1c3417834659f59a54c9080afac47
Maxbwee <maxbowen1234@gmail.com>
2022-11-18 12:57:45 +0200

    programs fetched from backend in results

--- src/views/ResultView.js
+++ src/views/ResultView.js
@@ -5,6 +5,7 @@ import "../styles/ResultView.css";
 import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import resultRoomsStore from "../data/ResultRoomsStore";
+import resultProgramStore from "../data/ResultProgramStore";
 // Tee funktiolle muuttuja
 
 /*
@@ -18,21 +19,30 @@ export default ExampleComponent; */
 
 export default function () {
 
-  const store = resultRoomsStore;
+  const roomStore = resultRoomsStore;
+  const progStore = resultProgramStore;
+
   const [rooms, setRooms] = useState([]);
+  const [progs, setProgs] = useState([]);
 
-  useEffect(() => getResultData,[])
+  useEffect(() => getRoomsData,[])
+  useEffect(() => getProgramData,[])
 
 
-  const getResultData = async () => {
-    await store.fetchRooms(10002);
-    setRooms(store.rooms);
+  const getRoomsData = async () => {
+    await roomStore.fetchRooms(10002);
+    setRooms(roomStore.rooms);
+  }
+
+  const getProgramData = async () => {
+    await progStore.fetchNames(10002);
+    setProgs(progStore.getNames())
   }
 
   return (
     <>
       <Typography style={{color: "#F6E9E9", margin: 20}}>Aineryhmä</Typography>
-      <ProgramResult data={testData.programs} />
+      <ProgramResult data={testData.programs} programs={progs}/>
       <div style={{ width: "80%", margin: "auto" }}>
       <Typography style={{color: "#F6E9E9"}}>Huoneet</Typography>
       <Result data={rooms} dropdownData={testData.programs}/>
