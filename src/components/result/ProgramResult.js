cd8490eac350b28643523e9829646be3f45f19e7
jaakko <jaakko.aakula@gmail.com>
2022-11-20 14:33:51 +0200

    subjectsview uses data from backend

--- src/components/result/ProgramResult.js
+++ src/components/result/ProgramResult.js
@@ -14,13 +14,11 @@ import resultProgramStore from "../../data/ResultProgramStore";
 export default function ProgramResult(props) {
   const programs = props.data;
   const progStore = resultProgramStore;
+  const [subProg, setSubProg] = React.useState({});
   const [open, setOpen] = React.useState(false);
-  const [programId, setProgramId] = React.useState(0);
-  const [programName, setProgramName] = React.useState("");
-  const handleOpen = (id, name) => {
-    setProgramId(id);
+  const handleOpen = (prog) => {
+    setSubProg(prog);
     setOpen(true);
-    setProgramName(name);
   };
   const handleClose = () => setOpen(false);
 
@@ -52,12 +50,9 @@ export default function ProgramResult(props) {
           <Typography
             style={{ textAlign: "center", marginTop: "5%", color: "#F6E9E9" }}
           >
-            {programName} -subjects
+            {subProg.name} -subjects
           </Typography>
-          <Result
-            data={testData.subjects[programId]}
-            dropdownData={testData.rooms}
-          />
+          <Result data={subProg.subjects} dropdownData={testData.rooms} />
         </Box>
       </Modal>
       <Grid2
@@ -85,7 +80,7 @@ export default function ProgramResult(props) {
               <Grid2 xs={1.5}>
                 <InfoOutlinedIcon
                   sx={{ color: "white", fontSize: 20 }}
-                  onClick={() => handleOpen(prog.id, prog.name)}
+                  onClick={() => handleOpen(prog)}
                 >
                   {" "}
                 </InfoOutlinedIcon>
