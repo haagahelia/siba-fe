8aec23a6e4c1cf8992b47f1a07dc198a44a2cd73
jaakko <jaakko.aakula@gmail.com>
2022-11-23 11:39:58 +0200

    subjects rooms fetched from backend

--- src/data/ResultRoomsStore.js
+++ src/data/ResultRoomsStore.js
@@ -2,14 +2,24 @@ import axios from "axios";
 import React from "react";
 
 class ResultRoomsStore {
-  constructor(rooms) {
+  constructor(rooms, subRooms) {
     this.rooms = rooms;
+    this.subRooms = subRooms;
   }
 
   async fetchRooms(id) {
     await axios
       .get(`http://localhost:3001/api/allocation/${id}/rooms`)
       .then((data) => (this.rooms = data.data))
+      .catch((e) => console.error(e));
+  }
+
+  async fetchSubRooms(subjectId, allocationId) {
+    await axios
+      .get(
+        `http://localhost:3001/api/allocation/${allocationId}/rooms/${subjectId}`,
+      )
+      .then((data) => (this.subRooms = data.data))
       .catch((e) => console.error(e));
   }
 }
