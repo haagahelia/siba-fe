6799cb1e954c1662ef674ad0323b8db7f089fe27
PutkisDude <putkis@gmail.com>
2022-11-29 15:23:53 +0200

    route and base for the allocation subject fail view

---
+++ src/views/AllocationSubjectFailureView.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function AllocationSubjectFailureView() {
+  return (
+    <div>
+      <Typography style={{ color: "#F6E9E9", margin: 20 }}>
+        Opetukset joita ei voinut allokoida
+      </Typography>
+    </div>
+  );
+}
