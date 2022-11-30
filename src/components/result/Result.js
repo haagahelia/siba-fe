959563927358cf2782c7b8af7fac127d6e07fdd6
gitjona <89576059+gitjona@users.noreply.github.com>
2022-11-30 11:10:34 +0200

    Update Result.js

--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -26,6 +26,9 @@ export default function Result(props) {
           
           const color =
             progress > 100 ? "#FF1700" : progress < 80 ? "#FFE400" : "#06FF00";
+          
+          const textColor =
+                progress == 0 ? "white" : "black";
 
           return (
             <>
@@ -35,8 +38,9 @@ export default function Result(props) {
               </Grid2>
               <Grid2 xs={3}> 
                 <ProgressBar
+                  labelAlignment="left"
                   baseBgColor={"#272121"}
-                  labelColor={"black"}
+                  labelColor={textColor}
                   bgColor={color}
                   padding={"3px"}
                   completed={Math.round(progress)}
