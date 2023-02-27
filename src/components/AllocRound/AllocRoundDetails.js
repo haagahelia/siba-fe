cd574bd9f19ffcdda44a1a671102d1a666a5cfe3
Sakariye1 <89934587+Sakariye1@users.noreply.github.com>
2023-02-27 11:19:00 +0200

    details view for AllocRoud

---
+++ src/components/AllocRound/AllocRoundDetails.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Typography,
+  DialogActions,
+} from "@mui/material";
+import AlertBox from "../common/AlertBox";
+
+export default function AllocRoudDetails(props) {
+  const { open, setOpen, singleAllocRoud } = props;
+
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertOptions] = useState({
+    title: "This is alert title",
+    message: "This is an error alert — check it out!",
+    severity: "error",
+  });
+
+  return (
+    <div>
+      <AlertBox
+        alertOpen={alertOpen}
+        alertOptions={alertOptions}
+        open={setAlertOpen}
+      />
+      <Dialog open={open} onClose={() => setOpen(false)} width="400px">
+        <DialogTitle id="dialog-title">
+          {singleAllocRoud?.AllocRoudName}
+        </DialogTitle>
+        <DialogContent>
+          <DialogActions
+            sx={{ justifyContent: "space-evenly", padding: "16px" }}
+          ></DialogActions>
+          <DialogContentText>
+            <Grid
+              container
+              spacing={1}
+              column={14}
+              direction="column"
+              justifyContent="flex-start"
+              alignItems="flex-start"
+              padding={2}
+            >
+              <Grid item s={6}>
+                <Typography variant="subtitle1">
+                  Name:&nbsp;
+                  {singleAllocRoud?.name}
+                </Typography>
+              </Grid>
+              <Grid item s={6}>
+                <Typography variant="subtitle1">
+                  Description:&nbsp;
+                  {singleAllocRoud?.description}
+                </Typography>
+              </Grid>
+              <Grid item s={6}>
+                <Typography variant="subtitle1">
+                  Last modified:&nbsp;
+                  {singleAllocRoud?.lastModified}
+                </Typography>
+              </Grid>
+            </Grid>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
