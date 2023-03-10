e84cd6897287a35e17f15b2ef546e7369a5fecec
valju <juhani.valimaki@haaga-helia.fi>
2023-03-10 11:09:47 +0200

    Settings dao baseurl picked from env vars

--- src/ajax/dao_settings.ts
+++ src/ajax/dao_settings.ts
@@ -1,7 +1,8 @@
 import { Response, Settings } from "../types";
+const baseUrl = process.env.REACT_APP_BE_SERVER_BASE_URL;
 
 export const fetchSettings = async (): Promise<Response<Settings>> => {
-  const request = new Request("http://localhost:3001/api/setting", {
+  const request = new Request(`${baseUrl}/setting`, {
     method: "GET",
   });
 
