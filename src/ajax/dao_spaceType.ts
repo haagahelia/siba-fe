a2752d5f7a774a5a55bb0c16e0962191d7a04a4a
Pratistha Mishra <56651666+Pratistha13@users.noreply.github.com>
2023-03-16 10:43:44 +0200

    made changes to spacetype dao

--- src/ajax/dao_spaceType.ts
+++ src/ajax/dao_spaceType.ts
@@ -1,6 +1,18 @@
 import { Response, SpaceType } from "../types";
 const baseUrl = process.env.REACT_APP_BE_SERVER_BASE_URL;
 
+export const fetchSpacetypeForSelect = async (): Promise<
+  Response<SpaceType>
+> => {
+  const request = new Request(`${baseUrl}/spaceType/getSelectData`, {
+    method: "GET",
+  });
+
+  const response = await fetch(request);
+  const programs: SpaceType[] = await response.json();
+  return { success: response.ok, data: programs };
+};
+
 export const fetchSpacetypeData = async (): Promise<Response<SpaceType>> => {
   const request = new Request(`${baseUrl}/spaceType/getSelectData`, {
     method: "GET",
@@ -10,19 +22,4 @@ export const fetchSpacetypeData = async (): Promise<Response<SpaceType>> => {
   const programs: SpaceType[] = await response.json();
 
   return { success: response.ok, data: programs };
-};
-
-export const postNewSpaceType = async (
-  newSpaceType: SpaceType,
-): Promise<boolean> => {
-  const request = new Request(`${baseUrl}/spaceType/post`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newSpaceType),
-  });
-  const response = await fetch(request);
-  return response.ok;
 };
