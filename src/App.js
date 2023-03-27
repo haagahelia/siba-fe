f24661ea017c1a79fdfee40f6147c43818ab1dd3
valju <juhani.valimaki@haaga-helia.fi>
2023-03-27 12:05:55 +0300

    The 3s delay was moved to be before asking result views to update with resetcounter

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./styles/theme";
 import Nav from "./routers/Nav";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <ThemeProvider theme={theme}>
         <Nav />
       </ThemeProvider>
