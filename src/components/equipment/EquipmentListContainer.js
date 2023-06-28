d840bc6769d3b4c8d5ec0a7a2b89d5d9d3176ab7
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-06-28 18:29:51 +0300

    fixed another case of invalid props removing react warning, functionality remains

--- src/components/equipment/EquipmentListContainer.js
+++ src/components/equipment/EquipmentListContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import SingleEquipmentDialog from "./SingleEquipmentDialog";
 import EquipmentList from "./EquipmentList";
@@ -7,9 +7,16 @@ import { Card } from "@mui/material";
 
 export default function EquipmentListContainer(props) {
   const { getAllEquipments, equipmentList } = props;
+
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      <SingleEquipmentDialog getAllEquipments={getAllEquipments} />
+      <SingleEquipmentDialog
+        open={open}
+        setOpen={setOpen}
+        getAllEquipments={getAllEquipments}
+      />
       <Grid container rowSpacing={1}>
         <Card variant="outlined">
           <CardContent>
