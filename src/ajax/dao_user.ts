305046af059ba8bc08c0009c37b8fafda44bfdf6
Jesse Reunamo <112503770+jesreu@users.noreply.github.com>
2023-09-13 01:55:02 +0300

    Created a first userlistview based on the design from subjectview

--- src/ajax/dao_user.ts
+++ src/ajax/dao_user.ts
@@ -1,5 +1,5 @@
 import Logger from "../logger/logger";
-import { Response, User, UserLoggedIn } from "../types";
+import { Response, User, UserLoggedIn, Subject, ResponseFiner } from "../types";
 
 const baseUrl = process.env.REACT_APP_BE_SERVER_BASE_URL;
 
@@ -16,6 +16,25 @@ export const postNewUser = async (newUser: User): Promise<boolean> => {
   Logger.debug("postNewUser", request);
   const response = await fetch(request);
   return response.ok;
+};
+
+export const fetchAllUsers = async (): Promise<ResponseFiner<Subject>> => {
+  const request = new Request(`${baseUrl}/user/`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("sessionToken")}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  const response = await fetch(request);
+
+  if (response.status === 200) {
+    const users: Subject[] = await response.json();
+    return { httpStatus: response.status, data: users };
+  } else {
+    return { httpStatus: response.status, data: [] };
+  }
 };
 
 export const getUserByEmail = async (
