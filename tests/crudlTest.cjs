7b9f6bd6b47418ee1f46d1f5a4785ff355faef9b
AkiAleksi <aki@hietamaki.org>
2023-11-13 17:00:08 +0200

    yhteys työhön

--- tests/crudlTest.cjs
+++ tests/crudlTest.cjs
@@ -1,25 +1,17 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
-try {
-  await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
-  let title = await driver.getTitle();
-  await driver.manage().setTimeouts({implicit: 500});
-  let textBox = await driver.findElement(By.name('my-text'));
-  let submitButton = await driver.findElement(By.css('button'));
-  await textBox.sendKeys('Selenium');
-  console.log(title)
-} finally {
-  //await driver.quit();
-}
-  /*
   try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.get('http://localhost:8753/');
+    let title = await driver.getTitle();
+    await driver.manage().setTimeouts({ implicit: 500 });
+    let textBox = await driver.findElement(By.name('my-text'));
+    let submitButton = await driver.findElement(By.css('button'));
+    await textBox.sendKeys('nyt');
+    console.log(title);
   } finally {
-    await driver.quit();
+    // Uncomment the following line to close the browser after the test
+    // await driver.quit();
   }
-  */
 })();
