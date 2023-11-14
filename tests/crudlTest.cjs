7b03579da23b8ce1fc0375c471c6ea59167d38cf
AkiAleksi <aki@hietamaki.org>
2023-11-14 12:44:41 +0200

    testing-login-admin

--- tests/crudlTest.cjs
+++ tests/crudlTest.cjs
@@ -1,17 +1,43 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { Builder, By, until } = require('selenium-webdriver');
+const assert = require('assert');
 
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://localhost:8753/');
+    await driver.get('http://localhost:8753/login');
     let title = await driver.getTitle();
     await driver.manage().setTimeouts({ implicit: 500 });
-    let textBox = await driver.findElement(By.name('my-text'));
-    let submitButton = await driver.findElement(By.css('button'));
-    await textBox.sendKeys('nyt');
-    console.log(title);
+
+    // Login
+    let loginEmail = await driver.findElement(By.id('loginEmail'));
+    await loginEmail.sendKeys('admin');
+
+    let loginPassword = await driver.findElement(By.id('loginPassword'));
+    await loginPassword.sendKeys('admin');
+
+    let submitButton = await driver.findElement(By.id('loginButton'));
+    await submitButton.click();
+
+    await driver.wait(until.urlIs('http://localhost:8753/subject'), 5000);
+
+    //test login
+    const currentUrl = await driver.getCurrentUrl();
+    assert.equal(currentUrl, 'http://localhost:8753/subject', 'URL is as expected');
+
+    // Log Out
+    let adminLink = await driver.findElement(By.linkText('admin'));
+    await adminLink.click();
+
+    let logOut = await driver.findElement(By.linkText('Log Out'));
+    await logOut.click();
+
+    //Test log out
+    const currentLogOutUrl = await driver.getCurrentUrl();
+    assert.equal(currentLogOutUrl, 'http://localhost:8753/login', 'URL is as expected');
+
   } finally {
     // Uncomment the following line to close the browser after the test
-    // await driver.quit();
+    await driver.quit();
   }
 })();
+
