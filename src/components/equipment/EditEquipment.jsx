394dcc7acbd6bea93022475e39cefa10abc10253
hollowdll <pixerchat@gmail.com>
2024-02-05 12:14:15 +0200

    add confirm dialog for edit equipment

--- src/components/equipment/EditEquipment.jsx
+++ src/components/equipment/EditEquipment.jsx
@@ -4,6 +4,7 @@ import dao from "../../ajax/dao";
 import Logger from "../../logger/logger";
 import ValidateEditEquipment from "../../validation/ValidateEditEquipment";
 import AlertBox from "../common/AlertBox";
+import ConfirmationDialog from "../common/ConfirmationDialog";
 import EditEquipmentForm from "./EditEquipmentForm";
 
 export default function EditEquipment({
@@ -15,8 +16,14 @@ export default function EditEquipment({
 
   const [alertOpen, setAlertOpen] = useState(false);
   const [alertOptions, setAlertOptions] = useState({
-    message: "This is an error alert — check it out!",
+    message: "alert message",
+    title: "alert title",
     severity: "error",
+  });
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogOptions, setDialogOptions] = useState({
+    title: "this is dialog",
+    content: "Something here",
   });
 
   const formik = useFormik({
@@ -25,7 +32,11 @@ export default function EditEquipment({
     validateOnChange: true,
     validate: ValidateEditEquipment,
     onSubmit: (values) => {
-      submitEditedEquipment(values);
+      setDialogOptions({
+        title: `Are you sure you want to edit ${values.name}?`,
+        content: `Press continue to save ${values.name} new information.`,
+      });
+      setDialogOpen(true);
     },
   });
 
@@ -58,6 +69,13 @@ export default function EditEquipment({
         alertOpen={alertOpen}
         alertOptions={alertOptions}
         setAlertOpen={setAlertOpen}
+      />
+      <ConfirmationDialog
+        dialogOpen={dialogOpen}
+        dialogOptions={dialogOptions}
+        setDialogOpen={setDialogOpen}
+        submit={submitEditedEquipment}
+        submitValues={formik.values}
       />
       <EditEquipmentForm formik={formik} />
     </div>
