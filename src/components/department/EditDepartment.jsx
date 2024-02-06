3cbe3963f5c356148e8936a624906a0e25a94909
hollowdll <pixerchat@gmail.com>
2024-02-06 15:36:56 +0200

    add ConfirmationDialog for EditDepartment

--- src/components/department/EditDepartment.jsx
+++ src/components/department/EditDepartment.jsx
@@ -4,13 +4,14 @@ import { validate } from "../../validation/ValidateAddEditDepartment";
 
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import useTheme from "@mui/material/styles/useTheme";
 import AlertBox from "../common/AlertBox";
+import ConfirmationDialog from "../common/ConfirmationDialog";
 
 export default function EditDepartment({
   singleDepartment,
@@ -28,15 +29,20 @@ export default function EditDepartment({
     message: "This is an error alert — check it out!",
     severity: "error",
   });
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogOptions, setDialogOptions] = useState({
+    title: "this is dialog",
+    content: "Something here",
+  });
 
   const theme = useTheme();
 
-  const submitEdits = async () => {
-    const validation = validate(department);
+  const submitEdits = async (submitValues) => {
+    const validation = validate(submitValues);
     if (Object.values(validation).length !== 0) {
-      alert(Object.values(validation));
+      alert(Object.values(submitValues));
     } else {
-      const result = await dao.editDepartment(department);
+      const result = await dao.editDepartment(submitValues);
       if (!result) {
         setAlertOptions({
           severity: "error",
@@ -49,7 +55,7 @@ export default function EditDepartment({
         setAlertOptions({
           severity: "success",
           title: "Success!",
-          message: `${department.name} updated successfully.`,
+          message: `${submitValues.name} updated successfully.`,
         });
         setAlertOpen(true);
         setEditOpen(false);
@@ -68,6 +74,13 @@ export default function EditDepartment({
         alertOptions={alertOptions}
         setAlertOpen={setAlertOpen}
       />
+      <ConfirmationDialog
+        dialogOpen={dialogOpen}
+        dialogOptions={dialogOptions}
+        setDialogOpen={setDialogOpen}
+        submit={submitEdits}
+        submitValues={department}
+      />
       <Button
         variant="contained"
         style={theme.components.MuiButton.editbutton}
@@ -80,37 +93,56 @@ export default function EditDepartment({
       <Dialog open={editOpen} onClose={() => setEditOpen(false)}>
         <DialogTitle>Edit Department</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            <Grid container variant="sibaGridEdit" spacing={3} column={7}>
-              <Grid item xs={12}>
-                <TextField
-                  name="Department"
-                  label="Department"
-                  defaultValue={singleDepartment?.name}
-                  onChange={(e) =>
-                    setDepartment({ ...department, name: e.target.value })
-                  }
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  name="Description"
-                  label="Description"
-                  defaultValue={singleDepartment?.description}
-                  onChange={(e) =>
-                    setDepartment({
-                      ...department,
-                      description: e.target.value,
-                    })
-                  }
-                />
-              </Grid>
+          <Grid container variant="sibaGridEdit" spacing={3} column={7}>
+            <Grid item xs={12}>
+              <TextField
+                name="Department"
+                label="Department"
+                defaultValue={singleDepartment?.name}
+                onChange={(e) =>
+                  setDepartment({ ...department, name: e.target.value })
+                }
+              />
             </Grid>
-            <Button onClick={submitEdits} variant="contained">
-              Submit
-            </Button>
-          </DialogContentText>
+            <Grid item xs={12}>
+              <TextField
+                name="Description"
+                label="Description"
+                defaultValue={singleDepartment?.description}
+                onChange={(e) =>
+                  setDepartment({
+                    ...department,
+                    description: e.target.value,
+                  })
+                }
+              />
+            </Grid>
+          </Grid>
         </DialogContent>
+        <DialogActions sx={{ justifyContent: "space-evenly" }}>
+          <Button
+            onClick={() => {
+              setEditOpen(false);
+            }}
+            variant="contained"
+            color="red"
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setEditOpen(false);
+              setDialogOptions({
+                title: `Are you sure you want to edit ${department.name}?`,
+                content: `Press continue to save ${department.name} new information.`,
+              });
+              setDialogOpen(true);
+            }}
+          >
+            Submit
+          </Button>
+        </DialogActions>
       </Dialog>
     </div>
   );
