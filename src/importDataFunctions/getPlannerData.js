da6c6577cc480b81668f7049e96ffe8c26d31218
Olli Salo <ullebror@gmail.com>
2024-03-01 10:38:24 +0200

    Moved the functions to importDataFunctions

---
+++ src/importDataFunctions/getPlannerData.js
@@ -0,0 +1,39 @@
+const getPlannerData = async (allocRoundId, sheetcolumns, plannerReport) => {
+  const { success, data } = await dao.fetchPlannerData(allocRoundId);
+
+  console.log(data);
+  if (!success) {
+    setAlertOptions({
+      severity: "error",
+      title: "Error",
+      message:
+        "Something went wrong with report data - please try again later.",
+    });
+
+    return;
+  }
+  const plannersheet = plannerReport.addWorksheet("Planner");
+  plannersheet.columns = sheetcolumns;
+
+  for (const row of data) {
+    plannersheet.addRow(row);
+  }
+
+  plannersheet.getRow(1).eachCell((cell) => {
+    cell.font = { bold: true };
+  });
+  try {
+    const buffer = await plannerReport.xlsx.writeBuffer();
+    const fileType =
+      "application/vnd.openxmlformats-officedocument.sreadsheetml.sheet";
+    const fileExtension = ".xlsx";
+
+    const blob = new Blob([buffer], { type: fileType });
+
+    saveAs(blob, `Your-Allocated-Lessons${fileExtension}`);
+    plannerReport.removeWorksheet(plannersheet.id);
+  } catch (err) {
+    console.log(`Could not download report: ${err}`);
+    plannerReport.removeWorksheet(plannersheet.id);
+  }
+};
