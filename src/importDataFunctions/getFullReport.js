cead45bd4ddbedad03d33195294b200885d8d158
Olli Salo <ullebror@gmail.com>
2024-03-11 11:21:26 +0200

    Created a function for full report

---
+++ src/importDataFunctions/getFullReport.js
@@ -0,0 +1,45 @@
+import * as ExcelJS from "Exceljs";
+import dao from "../ajax/dao";
+
+export const getFullReport = async (sheetcolumns, saveAs, setAlertOptions) => {
+  const { success, data } = await dao.fetchFullReportData();
+  if (!success) {
+    setAlertOptions({
+      severity: "error",
+      title: "Error",
+      message:
+        "Something went wrong with report data - please try again later.",
+    });
+
+    return;
+  }
+  const fullReport = new ExcelJS.Workbook();
+  const reportsheet = fullReport.addWorksheet("Report");
+  reportsheet.columns = sheetcolumns;
+
+  for (const row of data) {
+    reportsheet.addRow(row);
+  }
+
+  reportsheet.getRow(1).eachCell((cell) => {
+    cell.font = { bold: true };
+  });
+  try {
+    const buffer = await fullReport.xlsx.writeBuffer();
+    const fileType =
+      "application/vnd.openxmlformats-officedocument.sreadsheetml.sheet";
+    const fileExtension = ".xlsx";
+
+    const blob = new Blob([buffer], { type: fileType });
+    const today = new Date();
+    const currDate = `${today.getFullYear()}-${
+      today.getMonth() + 1
+    }-${today.getDate()}-${today.getHours()}.${today.getMinutes()}`;
+
+    saveAs(blob, `Full-Report${currDate}${fileExtension}`);
+    fullReport.removeWorksheet(reportsheet.id);
+  } catch (err) {
+    console.log(`Could not download report: ${err}`);
+    fullReport.removeWorksheet(reportsheet.id);
+  }
+};
