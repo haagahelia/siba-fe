a92064dde1e4125b4275b09326646a039d6810bc
Olli Salo <ullebror@gmail.com>
2024-05-16 10:36:25 +0300

    Created conditional rendering for isNoisy in AllocationSubjectFailureView

--- src/views/AllocationSubjectFailureView.jsx
+++ src/views/AllocationSubjectFailureView.jsx
@@ -241,10 +241,14 @@ export default function AllocationSubjectFailureView() {
                     Space type (Required:{" "}
                     {getNameForId(spaceNamesArray, unAllocSubject.spaceTypeId)})
                   </TableCell>
-                  <TableCell>
-                    Low Noise:
-                    {unAllocSubject.isNoisy === 1 ? " Not allowed" : " OK"}
-                  </TableCell>
+                  {unAllocSubject.isNoisy === 1 ? (
+                    <TableCell>
+                      Low Noise:
+                      {unAllocSubject.isNoisy === 1 ? " Not allowed" : ""}
+                    </TableCell>
+                  ) : (
+                    ""
+                  )}
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -308,21 +312,25 @@ export default function AllocationSubjectFailureView() {
                           </TableCell>
                         )}
                       </Tooltip>
-                      <Tooltip
-                        disableFocusListener
-                        title={`Low Noise: ${row.isLowNoise} 
+                      {unAllocSubject.isNoisy === 1 ? (
+                        <Tooltip
+                          disableFocusListener
+                          title={`Low Noise: ${row.isLowNoise} 
                         (${unAllocSubject.isNoisy === 1 ? " 0" : " Any"})`}
-                      >
-                        {unAllocSubject.isNoisy && row.isLowNoise === 1 ? (
-                          <TableCell>
-                            <CloseIcon className="redIcon" />
-                          </TableCell>
-                        ) : (
-                          <TableCell>
-                            <CheckIcon className="greenIcon" />
-                          </TableCell>
-                        )}
-                      </Tooltip>
+                        >
+                          {unAllocSubject.isNoisy && row.isLowNoise === 1 ? (
+                            <TableCell>
+                              <CloseIcon className="redIcon" />
+                            </TableCell>
+                          ) : (
+                            <TableCell>
+                              <CheckIcon className="greenIcon" />
+                            </TableCell>
+                          )}
+                        </Tooltip>
+                      ) : (
+                        ""
+                      )}
                     </TableRow>
                   ))
                 ) : (
